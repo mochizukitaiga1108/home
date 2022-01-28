2506a2ad768570050a54e7baa4db721e68b7d099
mochizukitaiga1108 <93418021+mochizukitaiga1108@users.noreply.github.com>
2022-01-28 23:28:57 +0900

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <head>
 	<title>Web</title>
-	<link href="style.css" rel="stylesheet" type="text/css">
+	<link href="style.css" rel="stylesheet">
 </head>
 
 <body>
@@ -28,7 +28,7 @@
 				<a href="https://www.nicovideo.jp/" target="_blank">
 					<img class="icon" src="icon/niconico.png">
 				</a>
-				<a href="https://www.nicovideo.jp/" target="_blank">ニコニコ</a>
+				<a href="https://www.nicovideo.jp/" target="_blank">NicoNico</a>
 			</div>
 			<div class="link">
 				<a href="https://www.amazon.co.jp/" target="_blank">
@@ -49,7 +49,7 @@
 			<div class="search">
 				<div class="form">
 					<form method="get" action="http://www.google.co.jp/search" target="_blank">
-						<img class="icon" src="icon/google.png">
+						<a href="https://www.google.co.jp/" target="_blank"><img class="icon" src="icon/google.png"></a>
 						<input type="text" name="q" maxlength="255">
 						<input type="hidden" name="ie" value="UTF-8">
 						<input type="hidden" name="oe" value="UTF-8">
@@ -60,7 +60,7 @@
 				<div class="space"></div>
 				<div class="form">
 					<form action="http://ja.wikipedia.org/w/index.php" target="_blank">
-						<img class="icon" src="icon/wiki.png">
+						<a href="https://ja.wikipedia.org/" target="_blank"><img class="icon" src="icon/wiki.png"></a>
 						<input type="text" name="search">
 						<input type="submit" name="fulltext" value="全文">
 						<input type="submit" name="go" value="単語">
@@ -69,7 +69,7 @@
 				<div class="space"></div>
 				<div class="form">
 					<form method="get" action="http://www.amazon.co.jp/gp/search" target="_blank">
-						<img class="icon" src="icon/amazon_or.png">
+						<a href="https://www.amazon.co.jp/" target="_blank"><img class="icon" src="icon/amazon_or.png"></a>
 						<input type="text" name="keywords">
 						<input type="submit" name="submit" value="検索">
 						<input type="hidden" name="tag" value="shihoriwish-22">
@@ -78,7 +78,7 @@
 				<div class="space"></div>
 				<div class="form">
 					<form method="get" action="https://www.youtube.com/search" target="_blank">
-						<img class="icon" src="icon/youtube_or.png">
+						<a href="https://www.youtube.com/" target="_blank"><img class="icon" src="icon/youtube_or.png"></a>
 						<input type="text" name="search_query">
 						<input type="submit" name="btnY" value="検索">
 					</form>
@@ -87,8 +87,16 @@
 			<div class="tab_bar">
 			<!-- ============================== タブ ============================== -->
 				<div class="tab">
-					<img class="icon" src="icon/home.png">
-					<span class="title">Home</span>
+					<img class="icon" src="icon/news.png">
+					<span class="title">　News　</span>
+				</div>
+				<div class="tab_action">
+					<a href="youtube.php"><img class="icon" src="icon/douga.png">
+					<span class="title">　Youtube　</span></a>
+				</div>
+				<div class="tab_action">
+					<a href="tool.php"><img class="icon" src="icon/tool.png">
+					<span class="title">　Tool　</span></a>
 				</div>
 			</div>
 		</div>
@@ -108,7 +116,7 @@
 				}
 			?>
 			<div class="container">
-				<h1>ＹＡＨＯＯ！天気・災害</h1>
+				<a href="https://weather.yahoo.co.jp/weather/" target="_blank"><img class="icon" src="icon/yahoo_ten.png"></a>
 				<div class="con_link">
 					<p><a href="<?php echo $data[0]['link']; ?>" target="_blank"> <?php echo $data[0]['title']; ?></a></p>
 				</div>
@@ -153,7 +161,7 @@
 				}
 			?>
 			<div class="container">
-				<h1>ＹＡＨＯＯ！ニュース</h1>
+				<a href="https://news.yahoo.co.jp/" target="_blank"><img class="icon" src="icon/yahoo_new.png"></a>
 				<div class="con_link">
 					<p><a href="<?php echo $data[0]['link']; ?>" target="_blank"> <?php echo $data[0]['title']; ?></a></p>
 				</div>
@@ -200,7 +208,7 @@
 				}
 			?>
 			<div class="container">
-				<h1>ＬＩＦＥＨＡＣＫＥＲ</h1>
+				<a href="https://www.lifehacker.jp/" target="_blank"><img class="icon" src="icon/life_hack.png"></a>
 				<div class="con_link">
 					<p><a href="<?php echo $data[0]['link']; ?>" target="_blank"> <?php echo $data[0]['title']; ?></a></p>
 				</div>
@@ -245,7 +253,7 @@
 				}
 			?>
 			<div class="container">
-				<h1>ＧＩＺＭＯＤＯ</h1>
+				<a href="https://www.gizmodo.jp/" target="_blank"><img class="icon" src="icon/giz_mod.png"></a>
 				<div class="con_link">
 					<p><a href="<?php echo $data[0]['link']; ?>" target="_blank"> <?php echo $data[0]['title']; ?></a></p>
 				</div>
@@ -279,8 +287,4 @@
 			</div>
 		</div>
 	</div>
-	
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-	<script type="text/javascript" src="rss.js"></script>
-	
 </body>
